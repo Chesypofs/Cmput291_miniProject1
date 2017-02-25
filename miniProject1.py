d6bcf691cad2c8719cc1edf59f2116df0db5e13d
William Hodgson <whodgson@ualberta.ca>
2017-02-25 12:11:47 -0700

    Login and Create Account

---
+++ miniProject1.py
@@ -0,0 +1,119 @@
+import cx_Oracle
+import sys
+import random
+
+def getConnection():
+	f = open('connection.txt')
+	username = f.readline().strip()
+	password = f.readline().strip()
+	f.close()
+	try:
+		return cx_Oracle.connect(username, password, "gwynne.cs.ualberta.ca:1521/CRS")
+	except cx_Oracle.DatabaseError as exc:
+		error = exc.args
+		print(sys.stderr, "Oracle code:", error.code)
+		print(sys.stderr, "Oracle message:", error.message)
+		sys.exit()
+		
+def login(connection):
+	while (True):
+		user_id = input("Please input your user id: ")
+		try:
+			user_id = int(user_id)
+			break
+		except ValueError:
+			print("User id must be an integer.")
+	user_password = input("Please input your password: ")
+	curs = connection.cursor()
+	curs.prepare("select * from users where usr = :id and pwd = :password")
+	curs.execute(None, {'id':user_id, 'password':user_password})
+	if curs.fetchone():
+		return user_id
+	else:
+		return False
+		
+def createAccount(connection):
+	user_name = ""
+	user_email = ""
+	user_city = ""
+	user_timezone = 0
+	user_password = ""
+	user_id = random.randrange(-2147483648, 2147483647) #-2^31 to (2^31)-1
+	while (True):
+		user_name = input("Please input a name: ")
+		if len(user_name) > 20:
+			print("Maximum length of name is 20.")
+		else:
+			break
+	while (True):
+		user_email = input("Please enter an email: ")
+		if len(user_email) > 15:
+			print("Maximum length of email is 15.")
+		else:
+			break
+	while (True):
+		user_city = input("Please enter a city: ")
+		if len(user_city) > 12:
+			print("Maximum length of city is 12.")
+		else:
+			break
+	while (True):
+		user_timezone = input("Please enter a timezone: ")
+		try:
+			user_timezone = float(user_timezone)
+			break
+		except ValueError:
+			print("Timezone must be a float.")
+	while (True):
+		user_password = input("Please enter a password: ")
+		if len(user_password) > 4:
+			print("Maximum length of password is 4.")
+		else:
+			break
+			
+	# Check that the user id is unique
+	while (True):
+		curs = connection.cursor()
+		curs.prepare("select * from users where usr = :id")
+		curs.execute(None, {'id':user_id})
+		if curs.fetchone():
+			user_id = random.randrange(-2147483648, 2147483647)
+			curs.close()
+		else:
+			print("User id is: ", user_id)
+			curs.close()
+			break
+	
+	curs = connection.cursor()
+	curs.prepare("insert into users values (:id, :pwd, :name, :email, :city, :timezone)")
+	curs.execute(None, {'id':user_id, 'pwd':user_password, 'name':user_name, 'email':user_email, 'city':user_city, 'timezone':user_timezone})
+	curs.close()
+	return user_id
+
+def main():
+	connection = getConnection()
+	user_id = False
+	while (True):
+		inp = input("Type 'login' to login, 'create' to create an account, or 'exit' to exit: ")
+		if inp == "exit":
+			connection.close()
+			sys.exit()
+		elif inp == "login":
+			user_id = login(connection)
+			if (user_id == False):
+				print("Invalid user id/password.")
+			else:
+				print("Successfully logged in.")
+				break
+		elif inp == "create":
+			user_id = createAccount(connection)
+			print("Successfully created an account and logged in.")
+			break
+		else:
+			print("Unrecognized input, please try again.")
+		
+	connection.commit()
+	connection.close()
+
+if __name__ == "__main__":
+	main()
